e0a8e4796c870bd7ee499e01f47afa4382f8ebb4
roschel <joaomroschel@gmail.com>
2023-02-06 10:47:48 -0300

    using context in html template

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request=request, template_name="recipes/home.html")
+    return render(
+        request=request, template_name="recipes/home.html", context={"name": "João"}
+    )
 
 
 def contato(request):
