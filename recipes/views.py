7b78bf6b01a878ebecee3589c2216f9034a216c7
roschel <joaomroschel@gmail.com>
2023-02-11 16:52:30 -0300

    filtrando por is_published

--- recipes/views.py
+++ recipes/views.py
@@ -8,7 +8,7 @@ from .models import Recipe
 
 # Create your views here.
 def home(request):
-    recipes = Recipe.objects.all().order_by("-id")
+    recipes = Recipe.objects.filter(is_published=True).order_by("-id")
     return render(
         request=request,
         template_name="recipes/pages/home.html",
@@ -17,10 +17,12 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by("-id")
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True
+    ).order_by("-id")
     return render(
         request=request,
-        template_name="recipes/pages/home.html",
+        template_name="recipes/pages/category.html",
         context={"recipes": recipes},
     )
 
