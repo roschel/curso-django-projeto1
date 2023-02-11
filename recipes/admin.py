b5d4986a115c2c7c4f2c0cd6fb012ca2901b7a9f
roschel <joaomroschel@gmail.com>
2023-02-11 14:34:45 -0300

    adicionando recipe no admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Category
+
+from .models import Category, Recipe
 
 # Register your models here.
 admin.site.register(Category)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
--- recipes/models.py
+++ recipes/models.py
@@ -27,3 +27,6 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to="recipes/covers/%Y/%m/%d")
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.title
