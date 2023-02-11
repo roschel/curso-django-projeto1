c19e94e148acf7aaeec25543dc3fb1db9cdeb203
roschel <joaomroschel@gmail.com>
2023-02-11 13:23:12 -0300

    terminando models e adicionando biblioteca Pillow para imagens

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -13,6 +19,8 @@ class Recipe(models.Model):
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at - models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to="recipes/covers/%Y/%m/%d")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
