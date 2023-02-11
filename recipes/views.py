b0d97ae28aa553991f5302a6691366863f9a4e2f
roschel <joaomroschel@gmail.com>
2023-02-11 10:49:01 -0300

    incluindo dados com Faker

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 # Create your views here.
 def home(request):
     return render(
         request=request,
         template_name="recipes/pages/home.html",
-        context={"name": "João"},
+        context={"recipes": [make_recipe() for _ in range(10)]},
     )
 
 
@@ -15,5 +17,5 @@ def recipe(request, id):
     return render(
         request=request,
         template_name="recipes/pages/recipe_view.html",
-        context={"name": "João"},
+        context={"recipes": make_recipe()},
     )
