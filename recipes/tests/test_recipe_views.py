05ed7d23ad53dba840ec0f1b9b4a8f93fb2aac66
roschel <joaomroschel@gmail.com>
2023-02-22 16:28:10 -0300

    assertIn

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -5,10 +5,6 @@ from recipes import views
 
 
 class RecipeViewsTest(TestCase):
-    def test_recipe_home_view_function_is_correct(self):
-        view = resolve(reverse("recipes:home"))
-        self.assertIs(view.func, views.home)
-
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(reverse("recipes:category", kwargs={"category_id": 1}))
         self.assertIs(view.func, views.category)
@@ -17,6 +13,10 @@ class RecipeViewsTest(TestCase):
         view = resolve(reverse("recipes:recipe", kwargs={"id": 1}))
         self.assertIs(view.func, views.recipe)
 
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse("recipes:home"))
+        self.assertIs(view.func, views.home)
+
     def test_recipe_home_view_returns_status_code_200_ok(self):
         response = self.client.get(path=reverse("recipes:home"))
         self.assertEqual(response.status_code, 200)
@@ -24,3 +24,7 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(path=reverse("recipes:home"))
         self.assertTemplateUsed(response, "recipes/pages/home.html")
+
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipe(self):
+        response = self.client.get(path=reverse("recipes:home"))
+        self.assertIn("No recipes found", response.content.decode(response.charset))
