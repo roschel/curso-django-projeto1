538a5ab51635af8baf2da951a313d863d5b57427
roschel <joaomroschel@gmail.com>
2023-02-22 14:25:13 -0300

    reorganizando testes e testando status code

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -4,21 +4,6 @@ from django.urls import resolve, reverse
 from recipes import views
 
 
-# Create your tests here.
-class RecipeURLsTest(TestCase):
-    def test_recipe_home_url_is_correct(self):
-        url = reverse("recipes:home")
-        self.assertEqual(url, "/")
-
-    def test_recipe_category_url_is_correct(self):
-        url = reverse("recipes:category", kwargs={"category_id": 1})
-        self.assertEqual(url, "/recipes/category/1")
-
-    def test_recipe_detail_url_is_correct(self):
-        url = reverse("recipes:recipe", kwargs={"id": 1})
-        self.assertEqual(url, "/recipes/1")
-
-
 class RecipeViewsTest(TestCase):
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse("recipes:home"))
@@ -31,3 +16,11 @@ class RecipeViewsTest(TestCase):
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse("recipes:recipe", kwargs={"id": 1}))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_home_view_returns_status_code_200_ok(self):
+        response = self.client.get(path=reverse("recipes:home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(path=reverse("recipes:home"))
+        self.assertTemplateUsed(response, "recipes/pages/home.html")
